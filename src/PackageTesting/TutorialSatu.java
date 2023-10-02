ec0ac8470d188bb3c3be3a10e0e9945ad68e26c2
Audyari <audy123ari@gmail.com>
2023-10-02 09:55:51 +0700

    Tutorial Pertama Updeate 1

--- src/PackageTesting/TutorialSatu.java
+++ src/PackageTesting/TutorialSatu.java
@@ -1,13 +1,19 @@
 package PackageTesting;
 
 
+
 public class TutorialSatu {
 	
   public static void main(String[] args) {
 	  
-	  TampilanPertama tampil = new TampilanPertama();
-	  tampil.DataPertama();
-	  tampil.DataKedua();	  
+	  //TampilanPertama tampil = new TampilanPertama();
+	  //tampil.DataPertama();
+	  //tampil.DataKedua();	
+	  
+	  InterfaceHello hello = new TampilanPertama();
+	  hello.DataPertama();
+	  hello.DataKedua();
+	  
 	  
 	
 }
